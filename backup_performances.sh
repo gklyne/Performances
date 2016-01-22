cee7efd2c2d4d077359c9ef56f1d0eba1552b26e
Graham Klyne <gk-github@ninebynine.org>
2016-01-22 11:14:24 +0000

    Fix collection name in local references

--- backup_performances.sh
+++ backup_performances.sh
@@ -1,7 +1,7 @@
 # Commit and backup changes to performamnce data
 # $1 is commit message
 
-cd /home/annalist/annalist_site/c/Performance_defs
+cd /home/annalist/annalist_site/c/Performances
 git add .
 git commit -m "${1:-Daily automatic commit}"
 git push
